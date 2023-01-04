11a4cc29b8464b3e041f3715f35b8250f47b4d93
merev <merev.personal@gmail.com>
2023-01-04 03:22:44 +0200

    nginx container playbook added

--- shared/post-provision-scripts/run-test-apps.sh
+++ shared/post-provision-scripts/run-test-apps.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 # Execute this script from the ansible workspace directory (/home/vagrant/ansible).
 
+echo "* Deploy NGINX container ..."
+sudo ansible-playbook nginx-container.yml
+
 echo "* Deploy BGApp ..."
 sudo ansible-playbook bgapp.yml
